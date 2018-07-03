f801b07948df393c28fdf4992593f335005bbc77
stereolith <lukasmoench1113@gmail.com>
2018-07-03 13:17:54 +0200

    AJAX php - verbindung script und js

--- webinterface/script.js
+++ webinterface/script.js
@@ -1,1 +1,14 @@
-console.log("Test");
+export function sendImg() {
+	console.log("test");
+	var value = $( "#previewImg" ).attr('src');
+	console.log(value);
+	if ($(value).val() != 0) {
+	$.post("server.php", {
+	    variable:value
+	}, function(data) {
+	    if (data != "") {
+	        //alert('We sent Jquery string to PHP : ' + data);
+	    }
+	});
+	}
+}
---
+++ webinterface/server.php
@@ -0,0 +1,4 @@
+<?php 
+	$jqueryVariable = $_POST['variable'];
+	echo $jqueryVariable;
+?>
